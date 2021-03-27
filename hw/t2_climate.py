ead1cf5458d87d79d414e6f2433e2587f407c058
justinchang0630 <104087@st.fhjh.tp.edu.tw>
2021-03-27 15:20:31 +0800

    2021/3/20 hw3

--- hw/t2_climate.py
+++ hw/t2_climate.py
@@ -1,8 +1,11 @@
 """
-Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱, 
+Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱,
 　　　如果溫度<= 10 顯示:太冷, 其他：舒適:
 
-Show:Please input temperature:"
-Input1:40
-Output:It's too hot.
 """
+temperture=input(temperture=)
+if temperture>=40:
+    print(too hot)
+elif temperture<=10:
+    print(too cold)
+else print (comfortable)
