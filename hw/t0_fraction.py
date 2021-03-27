d6946521f8f677cc79b41181b2e84df51b0c52e5
justinchang0630 <104087@st.fhjh.tp.edu.tw>
2021-03-27 15:45:01 +0800

    2021/3/20 hw all

--- hw/t0_fraction.py
+++ hw/t0_fraction.py
@@ -1,8 +1,23 @@
-分子=input ('輸入分子:')
-分母=input ('輸入分母:')
-分子=int (分子)
-分母=int (分母)
-分子/分母=int (分子/分母)
-if 分子/分母= 350/450:
-    print('yes')
-else print('no')
+"""
+Topic:輸入分子及分母，確認是否等於 350/450:
+
+Show:Please input numerator"
+Input1:70
+
+show:Please input Denominator:
+Input2:90
+Output:True
+
+Input1:6
+Input2:9
+Output:False
+"""
+check = 350/450
+n = int(input('input numberator:'))
+d = int(input('input denominator:'))
+rey= n/d
+
+if rey == check:
+    print('true')
+else:
+    print('false')
--- hw/t2_climate.py
+++ hw/t2_climate.py
@@ -3,9 +3,11 @@ Topic:輸入溫度，如果溫度>=40度C,顯示: 太熱,
 　　　如果溫度<= 10 顯示:太冷, 其他：舒適:
 
 """
-temperture=input(temperture=)
-if temperture>=40:
-    print(too hot)
-elif temperture<=10:
-    print(too cold)
-else print (comfortable)
+temperture=input('temperture:')
+temperture=int(temperture)
+if temperture >= 40:
+    print('too hot')
+elif temperture <= 10:
+    print('too cold')
+else :
+    print('comfortable')
