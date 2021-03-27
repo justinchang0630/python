9d5ee71f89c3badc4f29e40b1fc1717991a9c502
justinchang0630 <104087@st.fhjh.tp.edu.tw>
2021-03-27 15:17:43 +0800

    2021/3/20 hw2

--- hw/t1_triangle.py
+++ hw/t1_triangle.py
@@ -1,22 +1,15 @@
-"""
+'''
 Topic:輸入三角形三邊，判斷是否能構成三角形，
 　　　是三角形則顯示面積和周長，不行則顯示，無法構成三角形:
+'''
+邊a= input(邊a)
+邊b= input(邊b)
+邊c= input(邊c)
+if (邊a+邊b>=邊c),(邊b+邊c>=邊a),(邊c+邊a>=邊b):
+    print(邊a+邊b+邊c)
+else print(no)
 
-Circle Area formula: 
-p = 1/2 (a+b+c)
-area = (p * (p - a) * (p - b) * (p - c)) ** 0.5
 
-e.g.
-Show:a ="
-Input1:3
 
-Show:b ="
-Input2:4
 
-Show:c ="
-Input3:5
 
-output:
-perimeter: 12.000000
-Area: 6.000000
-"""
